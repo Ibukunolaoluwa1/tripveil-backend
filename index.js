327947e9790648fb7c64f9e2194079b49cadaf66
Ibukunolaoluwa1 <olaflorence2310@gmail.com>
2026-06-08 16:48:57 +0100

    add check-in routes to index

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import mongoose from 'mongoose';
 import authRoutes from './router/authRoutes.js';
+import checkInRoutes from './router/checkin.routes.js';
 
 dotenv.config();
 
@@ -10,6 +11,8 @@ const app = express();
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
+
+app.use('/api/checkin', checkInRoutes);
 
 mongoose.connect(process.env.MONGO_URI)
 .then(() => {
